b8ab8fda54e7077edd652cbd7ae6e6f0c6aebd94
Shashank Chauhan <shashankc0706@gmail.com>
2024-07-04 14:54:53 +0530

    Update data_mem.v

--- modules/memory/data_mem.v
+++ modules/memory/data_mem.v
@@ -10,7 +10,7 @@ module data_mem #(parameter DATA_WIDTH=32, ADDR_WIDTH=32, MEM_SIZE=128)(
 	//128 32-bit words or data
 	reg [DATA_WIDTH-1:0] data_ram [0:MEM_SIZE-1];
 	
-  //word-aligned memory access
+        //word-aligned memory access
 	assign rd_data_mem = data_ram[addr];
 	
 	//synchronous write logic
