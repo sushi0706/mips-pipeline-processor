e7048094035f322312afa618733b53ca2f592b67
Shashank Chauhan <shashankc0706@gmail.com>
2024-07-07 11:53:00 +0530

    Update main_decoder.v

--- modules/control_unit/main_decoder.v
+++ modules/control_unit/main_decoder.v
@@ -16,12 +16,12 @@ module main_decoder(
 	always@(*) begin
 		case(opcode)
 		  6'b000000: controls <= 9'b110000010; //R-type
-        6'b100011: controls <= 9'b101001000; //LW
-        6'b101011: controls <= 9'b0x101x000; //SW
-        6'b000100: controls <= 9'b0x010x001; //BEQ
-        6'b001000: controls <= 9'b101000000; //ADDI
-        6'b000010: controls <= 9'b0xxx0x1xx; //J
-        default:   controls <= 9'bxxxxxxxxx;
+                  6'b100011: controls <= 9'b101001000; //LW
+                  6'b101011: controls <= 9'b0x101x000; //SW
+                  6'b000100: controls <= 9'b0x010x001; //BEQ
+                  6'b001000: controls <= 9'b101000000; //ADDI
+                  6'b000010: controls <= 9'b0xxx0x1xx; //J
+                  default:   controls <= 9'bxxxxxxxxx;
 		endcase
 	end
 	
